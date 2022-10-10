0fad10733553bf67a08ca7ce4cb9dce67c50a873
uSlashVlad <u.slash.vlad@gmail.com>
2022-10-10 10:33:41 +0300

    feature: Added "zero" icon and "fluttericons" config Added app's logo as icon of score points using "FlutterIcons" website

--- lib/units/home/presentation/view/components/stats_header.dart
+++ lib/units/home/presentation/view/components/stats_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:not_zero/helpers/not_zero_icons.dart';
 import 'package:not_zero/units/home/presentation/bloc/home_score_cubit.dart';
 import 'package:not_zero/units/home/presentation/bloc/states/home_score_state.dart';
 
@@ -26,20 +27,27 @@ class HomeStatsHeader extends StatelessWidget {
         ),
       ),
       padding: const EdgeInsets.only(bottom: 15),
-      child: BlocBuilder<HomeScoreCubit, HomeScoreState>(
-        builder: (context, state) {
-          if (state.totalPoints != null) {
-            return Text(
-              state.totalPoints.toString(),
-              style: const TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.w800,
-              ),
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          BlocBuilder<HomeScoreCubit, HomeScoreState>(
+            builder: (context, state) {
+              if (state.totalPoints != null) {
+                return Text(
+                  state.totalPoints.toString(),
+                  style: const TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.w700,
+                  ),
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+          const SizedBox(width: 6),
+          const Icon(NotZeroIcons.zero)
+        ],
       ),
     );
   }
