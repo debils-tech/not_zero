5b3a8642e6d8b4832c812c51ee1b0662de4a2da4
uSlashVlad <u.slash.vlad@gmail.com>
2022-10-12 23:00:11 +0300

    feature: Replaced displayed app name in config on most platforms Changes was tested on Android and Linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "not_zero");
+    gtk_header_bar_set_title(header_bar, "Not Zero");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "not_zero");
+    gtk_window_set_title(window, "Not Zero");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
