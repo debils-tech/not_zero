1f7a4684043b965244fd6c3c4b2229da276f3ca7
uSlashVlad <u.slash.vlad@gmail.com>
2022-10-18 22:49:21 +0300

    refactor(storage): Removed already useless strings

--- lib/constants/database.dart
+++ lib/constants/database.dart
@@ -1,11 +1,5 @@
-class LocalCollections {
-  LocalCollections._();
-
-  static const String tasks = 'tasks';
-}
-
 class SettingsKeys {
   SettingsKeys._();
 
-  static const String themeState = 'settings_themeState';
+  static const String themeState = 'themeState';
 }
