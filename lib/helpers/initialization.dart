2ef199c2f47b568e12323965a42fab92507fa0d3
uSlashVlad <u.slash.vlad@gmail.com>
2022-10-19 09:56:22 +0300

    fix: Fixed flutter analysis problem Related to TODO comment

--- lib/helpers/initialization.dart
+++ lib/helpers/initialization.dart
@@ -6,7 +6,8 @@ import 'package:not_zero/units/stats/domain/repositories/stats_repository.dart';
 class InitializationHelper {
   InitializationHelper._();
 
-  // TODO refactor this function because it might be useless with new database.
+  // TODO(uSlashVlad): refactor this function because it might be useless with
+  // new database.
   static Future<void> initDatabaseRelated() async {
     unawaited(getIt<StatsRepository>().countTotalPoints());
   }
