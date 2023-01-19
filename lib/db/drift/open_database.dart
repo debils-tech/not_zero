ac5a494bacceadb4c5636f7c378119bcf072a0bd
uSlashVlad <u.slash.vlad@gmail.com>
2023-01-19 10:27:30 +0300

    refactor: Removed import ignore

--- lib/db/drift/open_database.dart
+++ lib/db/drift/open_database.dart
@@ -1,5 +1,3 @@
-// Relative imports are usefull here because of conditional imports.
-// ignore: always_use_package_imports
 export 'unsupported_database.dart'
     if (dart.library.ffi) 'native_database.dart'
     if (dart.library.html) 'web_database.dart';
