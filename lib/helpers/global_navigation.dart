2e4132d962bd1ff5b15cbd7dab2d399486fb4d68
uSlashVlad <u.slash.vlad@gmail.com>
2023-01-27 00:24:06 +0300

    feature: Added Escape shortcut for closing screens

--- lib/helpers/global_navigation.dart
+++ lib/helpers/global_navigation.dart
@@ -4,4 +4,11 @@ abstract class GlobalNavigation {
   static final key = GlobalKey<NavigatorState>();
 
   static BuildContext get context => key.currentContext!;
+
+  static NavigatorState? get navigator {
+    final currentContext = key.currentContext;
+    if (currentContext == null) return null;
+
+    return Navigator.maybeOf(currentContext);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:not_zero/constants/themes.dart';
 import 'package:not_zero/db/provider.dart';
 import 'package:not_zero/get_it.dart';
+import 'package:not_zero/helpers/global_navigation.dart';
 import 'package:not_zero/helpers/licenses.dart';
+import 'package:not_zero/helpers/platform_extenstions.dart';
 import 'package:not_zero/i18n/translations.g.dart';
 import 'package:not_zero/routes.dart';
 import 'package:not_zero/units/settings/domain/models/theme_state.dart';
@@ -46,6 +50,13 @@ class MyApp extends StatelessWidget {
         return MaterialApp.router(
           title: 'Flutter Demo',
           debugShowCheckedModeBanner: false,
+          shortcuts: {
+            if (isPlatformDesktop || kIsWeb)
+              const SingleActivator(LogicalKeyboardKey.escape):
+                  VoidCallbackIntent(
+                () => GlobalNavigation.navigator?.maybePop(),
+              ),
+          },
 
           //-- Themes --
           themeMode: _stateToModMap[state],
