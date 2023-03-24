114bc7eaba83c9261d3d409689ca559763881c78
uSlashVlad <uslashvlad@bwg-io.site>
2023-03-25 00:11:45 +0300

    test(tags): Some additional tests

---
+++ test/tags/tags_db_config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:not_zero/db/provider.dart';
+import 'package:not_zero/get_it.dart';
+
+import 'template_tags.dart';
+
+void configDatabaseForTags() {
+  setUp(() async {
+    final db = getIt<StorageProvider>().database;
+    await db.transaction(() async {
+      for (final t in templateTags) {
+        await db.upsertIn(db.tagsTable, t.toInsertable());
+      }
+    });
+  });
+
+  tearDown(() => getIt<StorageProvider>().database.drop());
+}
