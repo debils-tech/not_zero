1c303595028e11571026395aa2e18457ad29894a
Vladislav Nagorny <u.slash.vlad@gmail.com>
2024-08-30 21:27:01 +0300

    refactor: Temporary removed backup-related features

--- lib/units/settings/di.dart
+++ lib/units/settings/di.dart
@@ -3,20 +3,8 @@ import 'package:not_zero/helpers/app_info.dart';
 import 'package:not_zero/units/settings/models/theme_state.dart';
 import 'package:not_zero/units/settings/notifiers/theme_state_notifier.dart';
 import 'package:not_zero/units/settings/repositories/settings_repository.dart';
-import 'package:not_zero/units/settings/services/backup_local_service.dart';
 import 'package:not_zero/units/settings/services/settings_local_service.dart';
 import 'package:not_zero/units/storage/di.dart';
-import 'package:not_zero/units/tags/di.dart';
-import 'package:not_zero/units/tasks/di.dart';
-
-final backupLocalServiceProvider = Provider<BackupLocalService>((ref) {
-  return BackupLocalService(
-    ref.watch(databaseProvider),
-    ref.watch(settingsBoxProvider),
-    ref.watch(tasksLocalServiceProvider),
-    ref.watch(tagsLocalServiceProvider),
-  );
-});
 
 final settingsLocalServiceProvider = Provider<SettingsLocalService>((ref) {
   return SettingsLocalService(
@@ -27,7 +15,6 @@ final settingsLocalServiceProvider = Provider<SettingsLocalService>((ref) {
 final settingsRepositoryProvider = Provider<SettingsRepository>((ref) {
   return SettingsRepository(
     ref.watch(settingsLocalServiceProvider),
-    ref.watch(backupLocalServiceProvider),
   );
 });
 
--- lib/units/settings/repositories/settings_repository.dart
+++ lib/units/settings/repositories/settings_repository.dart
@@ -1,18 +1,11 @@
 import 'package:logging/logging.dart';
-import 'package:not_zero/constants/database.dart';
-import 'package:not_zero/units/settings/models/backup_model.dart';
 import 'package:not_zero/units/settings/models/theme_state.dart';
-import 'package:not_zero/units/settings/services/backup_local_service.dart';
 import 'package:not_zero/units/settings/services/settings_local_service.dart';
 
 class SettingsRepository {
-  SettingsRepository(
-    this._settingsLocalService,
-    this._backupLocalService,
-  );
+  SettingsRepository(this._settingsLocalService);
 
   final SettingsLocalService _settingsLocalService;
-  final BackupLocalService _backupLocalService;
 
   final log = Logger('SettingsRepository');
 
@@ -22,61 +15,5 @@ class SettingsRepository {
 
   Future<void> saveThemeState(ThemeState state) {
     return _settingsLocalService.setThemeState(state);
-  }
-
-  Future<bool> exportData() async {
-    final Map<String, String> settings;
-    final List<Map<String, dynamic>> tasks;
-    final List<Map<String, dynamic>> tags;
-    try {
-      settings = _backupLocalService.getAllSettings();
-      tasks = await _backupLocalService.getAllTasks();
-      tags = await _backupLocalService.getAllTags();
-    } catch (e, s) {
-      log.severe('Error while getting info for export', e, s);
-      return false;
-    }
-
-    final backupContent = BackupModel(
-      version: 2,
-      // appInfo: getIt<AppInfo>(), // TODO(uSlashVlad): Return AppInfo here
-      data: {
-        BoxNames.settings: settings,
-        TableNames.tasks: tasks,
-        TableNames.tags: tags,
-      },
-    );
-
-    return _backupLocalService.exportDataToFile(backupContent);
-  }
-
-  Future<bool> importData() async {
-    final backupContent = await _backupLocalService.importDataFromFile();
-    if (backupContent == null) return false;
-
-    // If version is not supported
-    if (backupContent.version > 2 || backupContent.version < 1) return false;
-
-    try {
-      final rawSettings = backupContent.data[BoxNames.settings] as Map;
-      await _backupLocalService.applyAllSettings(
-        Map<String, String>.from(rawSettings),
-      );
-
-      final rawTags = backupContent.data[TableNames.tags] as List;
-      await _backupLocalService.applyAllTags(
-        rawTags.map((e) => Map<String, dynamic>.from(e as Map)).toList(),
-      );
-
-      final rawTasks = backupContent.data[TableNames.tasks] as List;
-      await _backupLocalService.applyAllTasks(
-        rawTasks.map((e) => Map<String, dynamic>.from(e as Map)).toList(),
-      );
-    } catch (e, s) {
-      log.severe('Error while applying backup', e, s);
-      return false;
-    }
-
-    return true;
   }
 }
