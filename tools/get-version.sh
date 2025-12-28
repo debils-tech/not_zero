27ba95450d6359eeae574d6fde92d7bbd9923b6b
Vladislav Nagorny <u.slash.vlad@gmail.com>
2025-12-28 15:12:55 +0300

    build: New version update logic for CI
    Now it only needs major.minor version and increments the patch

--- tools/get-version.sh
+++ tools/get-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat apps/not_zero_app/pubspec.yaml | grep "version: " | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+"
+(grep "version: " | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+") < apps/not_zero_app/pubspec.yaml
--- tools/update-version.sh
+++ tools/update-version.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-TAG_VERSION="$(echo "$1" | grep -Eo "^[0-9]+\.[0-9]+").$(echo "$2" | grep -Eo "^[0-9]+")"
+TARGET_MAJOR_MINOR=$(echo "$1" | grep -Eo "[0-9]+\.[0-9]+")
+PUBSPEC_PATH="apps/not_zero_app/pubspec.yaml"
+CURRENT_FULL_VERSION=$(grep '^version: ' $PUBSPEC_PATH | sed 's/version: //')
 
-CURRENT_VERSION=$(grep '^version: ' apps/not_zero_app/pubspec.yaml | sed 's/version: //')
-CURRENT_BUILD_NUMBER=$(echo "$CURRENT_VERSION" | cut -d'+' -f2)
+# Remove build number to get semver (e.g., "0.14.0")
+CURRENT_SEMVER=$(echo "$CURRENT_FULL_VERSION" | cut -d'+' -f1)
+# Extract Major.Minor (e.g., "0.14")
+CURRENT_MAJOR_MINOR=$(echo "$CURRENT_SEMVER" | cut -d'.' -f1,2)
+# Extract Patch (e.g., "0")
+CURRENT_PATCH=$(echo "$CURRENT_SEMVER" | cut -d'.' -f3)
+# Extract Build Number (e.g., "10")
+CURRENT_BUILD_NUMBER=$(echo "$CURRENT_FULL_VERSION" | cut -d'+' -f2)
 
-if [ -z "$CURRENT_BUILD_NUMBER" ]; then
+if [ -z "$CURRENT_BUILD_NUMBER" ] || [ "$CURRENT_BUILD_NUMBER" == "$CURRENT_SEMVER" ]; then
   CURRENT_BUILD_NUMBER=0
 fi
+
+if [ "$CURRENT_MAJOR_MINOR" == "$TARGET_MAJOR_MINOR" ]; then
+  # Versions match (e.g. 0.14 == 0.14)
+  # Increment Patch
+  NEW_PATCH=$((CURRENT_PATCH + 1))
+else
+  # Versions do not match (e.g. 0.13 != 0.14)
+  # Set Patch to 0
+  NEW_PATCH=0
+fi
+
 NEW_BUILD_NUMBER=$((CURRENT_BUILD_NUMBER + 1))
 
-sed -i "s/version: $CURRENT_VERSION/version: $TAG_VERSION+$NEW_BUILD_NUMBER/" apps/not_zero_app/pubspec.yaml
+NEW_TAG_VERSION="$TARGET_MAJOR_MINOR.$NEW_PATCH"
+NEW_FULL_VERSION="$NEW_TAG_VERSION+$NEW_BUILD_NUMBER"
 
-echo -e "app_version=$TAG_VERSION\n""app_build_number=$NEW_BUILD_NUMBER"
+sed -i "s/^version: .*/version: $NEW_FULL_VERSION/" $PUBSPEC_PATH
+
+echo "app_version=$NEW_TAG_VERSION"
+echo "app_build_number=$NEW_BUILD_NUMBER"
