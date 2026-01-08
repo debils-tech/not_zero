6a5ffb8f92034c25005b4526d45993508647d29a
Vladislav Nagorny <u.slash.vlad@gmail.com>
2026-01-08 18:01:25 +0300

    build: Configuring notifications plugin and monochrome icons for android

--- apps/not_zero_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/not_zero_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import dynamic_color
 import file_picker
+import flutter_local_notifications
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -16,6 +17,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
