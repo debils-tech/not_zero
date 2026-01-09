2bc11d7581ccc2c19b1ccbc80ef315b090112537
Vladislav Nagorny <u.slash.vlad@gmail.com>
2026-01-09 20:43:33 +0300

    fix: Android app wasn't starting because of missing `@drawable/ic_notification` file

--- apps/not_zero_app/lib/src/features/notifications/helpers/init_notification_helper.dart
+++ apps/not_zero_app/lib/src/features/notifications/helpers/init_notification_helper.dart
@@ -1,32 +1,44 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:logging/logging.dart';
 import 'package:not_zero_app/src/features/translations/translations.g.dart';
 import 'package:not_zero_app/src/helpers/app_info.dart';
 
 abstract final class InitNotificationHelper {
   static Future<void> initAppNotifications() async {
-    final appInfo = await AppInfo.fromEnvironment();
+    try {
+      final appInfo = await AppInfo.fromEnvironment();
 
-    const androidSettings = AndroidInitializationSettings(
-      '@drawable/ic_notification',
-    );
-    const darwinSettings = DarwinInitializationSettings();
-    final linuxSettings = LinuxInitializationSettings(
-      defaultActionName: t.common.notifications.linuxDefaultAction,
-    );
-    final windowsSettings = WindowsInitializationSettings(
-      appName: appInfo.name,
-      appUserModelId: appInfo.packageName,
-      guid: '9283b70d-6624-44fa-9075-3554fb56c4de',
-    );
+      const androidSettings = AndroidInitializationSettings(
+        '@drawable/ic_notification',
+      );
+      const darwinSettings = DarwinInitializationSettings();
+      final linuxSettings = LinuxInitializationSettings(
+        defaultActionName: t.common.notifications.linuxDefaultAction,
+      );
+      final windowsSettings = WindowsInitializationSettings(
+        appName: appInfo.name,
+        appUserModelId: appInfo.packageName,
+        guid: '9283b70d-6624-44fa-9075-3554fb56c4de',
+      );
 
-    await FlutterLocalNotificationsPlugin().initialize(
-      InitializationSettings(
-        android: androidSettings,
-        iOS: darwinSettings,
-        macOS: darwinSettings,
-        linux: linuxSettings,
-        windows: windowsSettings,
-      ),
-    );
+      await FlutterLocalNotificationsPlugin().initialize(
+        InitializationSettings(
+          android: androidSettings,
+          iOS: darwinSettings,
+          macOS: darwinSettings,
+          linux: linuxSettings,
+          windows: windowsSettings,
+        ),
+      );
+    } on Object catch (e, s) {
+      Logger(
+        'InitNotificationHelper',
+      ).severe(
+        'Error while initializing notifications! '
+        "Local notifications probably wouldn't work",
+        e,
+        s,
+      );
+    }
   }
 }
